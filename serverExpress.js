7e07e6c29258269e96c107febe7f7cfadc759f54
jj <inbox4development@gmail.com>
2019-02-13 14:22:22 -0500

    Adding Port Env

--- serverExpress.js
+++ serverExpress.js
@@ -249,7 +249,7 @@ app.post('/imageUrl', (req, res) => {image.handleAPICall(req, res)});
 //     .catch(err => res.status(400).json('unable to get entries'))
 // })
 
-app.listen(3000, ()=> {
-    console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, ()=> {
+    console.log('app is running on port ${process.env.PORT}');
 });
 
